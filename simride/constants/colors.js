fde506d9ddbf2f48a0e6b8e7df24585fa78e1de3
utyumba <vvi001@mymail.sim.edu.sg>
2020-04-27 17:06:27 +0800

    Color constants added

---
+++ simride/constants/colors.js
@@ -0,0 +1,7 @@
+export const COLORS = {
+    BLACK: '#000000',
+    GREEN_PRI: '#4b9ea1',
+    GREEN_SEC: '#4b87a1',
+    GREY: '#222222',
+    WHITE: '#FFFFFF',
+};
---
+++ simride/styles/index.js
@@ -0,0 +1,8 @@
+/**
+ * This file should export global style elements such as
+ * colors, button, fonts etc. , but should not have any
+ * component specific styling.
+ *
+ * All styles exported should just be plain javascript
+ * objects describing the styles they modify.
+ */
